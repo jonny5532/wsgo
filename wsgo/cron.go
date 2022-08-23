c96eea0c0fe5d7612c7476e95556c6abe9d81d06
Jonny <jonny@jonnybarker.com>
2022-08-23 10:56:09 +0100

    Another attempt to fix the cron scheduler

--- wsgo/cron.go
+++ wsgo/cron.go
@@ -117,68 +117,129 @@ func CronTick() {
 	}
 }
 
+func Iif(condition bool, ifTrue int, ifFalse int) int {
+	if condition {
+		return ifTrue
+	} else {
+		return ifFalse
+	}
+}
+
 func (cron *Cron) calculateNextRun() time.Time {
-	nextRun := time.Now()
+	now := time.Now()
 
 	if cron.period > 0 {
 		// Periodic tasks will slip in time, since the gap between the previous
 		// task ending and the next starting are the period, rather than the
 		// time between starts
-		return nextRun.Add(cron.period)
+		return now.Add(cron.period)
 	}
 
-	// FIXME - this is all pretty clumsy
+	// FIXME - wday doesn't work yet
 
-	var cur int
-	var next int
+	if false {
+		// for testing
+		now = time.Date(
+			now.Year(), 
+			now.Month(),
+			now.Day(),
+			7, 
+			21, 
+			0, 0, now.Location(),
+		)
+	}
+	
+	// Calculate the 'nextRun' based on the current time, replacing any of
+	// the time segments with those specified in the cron rule. The resulting
+	// time may be in the future (fine), but it may also be in the past, in
+	// which case it will need advancing.
 
-	if cron.mon > -1 {
-		v := nextRun.Month()
-		if time.Month(cron.mon) != v {
-			nextRun = time.Date(nextRun.Year(), time.Month(cron.mon), 1, 0, 0, 0, 0, nextRun.Location())
-			if time.Month(cron.mon) < v {
-				nextRun = nextRun.AddDate(1, 0, 0)
-			}
-		}
-	}
-	if cron.wday > -1 {
-		v := int(nextRun.Weekday())
-		if cron.wday != v {
-			nextRun = time.Date(nextRun.Year(), nextRun.Month(), nextRun.Day(), 0, 0, 0, 0, nextRun.Location())
-			nextRun = nextRun.AddDate(0, 0, (7+(cron.wday-int(nextRun.Weekday())))%7)
-		}
-	}
-	if cron.day > -1 {
-		v := nextRun.Day()
-		if cron.day != v {
-			nextRun = time.Date(nextRun.Year(), nextRun.Month(), cron.day, 0, 0, 0, 0, nextRun.Location())
-			if cron.day < v {
-				nextRun = nextRun.AddDate(0, 1, 0)
-			}
-		}
-	}
-	if cron.hour > -1 {
-		v := nextRun.Hour()
-		if cron.hour != v {
-			nextRun = time.Date(nextRun.Year(), nextRun.Month(), nextRun.Day(), cron.hour, 0, 0, 0, nextRun.Location())
-			if cron.hour < v {
-				nextRun = nextRun.AddDate(0, 0, 1)
-			}
-		}
-	}
-	if true {
-		cur = nextRun.Minute()
-		if cron.min < 0 {
-			// -1 means every minute, -2 every second minute, etc
-			next = (cur + (-cron.min) - (cur % -cron.min)) % 60
-		} else {
-			next = cron.min
-		}
-		nextRun = time.Date(nextRun.Year(), nextRun.Month(), nextRun.Day(), nextRun.Hour(), next, 0, 0, nextRun.Location())
-		if cron.hour == -1 && next <= cur {
+	nextRun := time.Date(
+		now.Year(), 
+		time.Month(Iif(cron.mon > -1, cron.mon, int(now.Month()))),
+		Iif(cron.day > -1, cron.day, now.Day()),
+		Iif(cron.hour > -1, cron.hour, now.Hour()), 
+		Iif(cron.min > -1, cron.min, now.Minute()), 
+		0, 0, now.Location(),
+	)
+
+	// handle wday - BROKEN
+	// if cron.wday > -1 && nextRun.Weekday() != (cron.wday%7) {
+	// 	nextRun := nextRun.AddDate(0, 0, (7+cron.wday-nextRun.Weekday())%7)
+	// }
+
+	// nextRun is currently in the past, so try advancing the minute (if it isn't specified)
+
+	if !nextRun.After(now) && cron.min == -1 {
+		// try the next minute
+		v := nextRun.Minute() + 1
+		nextRun = time.Date(nextRun.Year(), nextRun.Month(), nextRun.Day(), nextRun.Hour(), v%60, 0, 0, nextRun.Location())
+		if v >= 60 && cron.hour == -1 && (nextRun.Hour() < 23 || cron.day == -1) {
+			//rolled over, and hour is not specified, and day is either not specified or increment won't roll over, so add an hour
 			nextRun = nextRun.Add(1 * time.Hour)
 		}
 	}
+
+	// nextRun is still in the past, try advancing the hour if we're allowed
+
+	if !nextRun.After(now) && cron.hour == -1 {
+		// try the next hour
+		v := now.Hour() + 1
+		nextRun = time.Date(nextRun.Year(), nextRun.Month(), nextRun.Day(), 
+			v%24, 
+			Iif(cron.min > -1, cron.min, 0), // use minute if specified, else the start of the next hour
+			0, 0, now.Location(),
+		)
+		if v >= 24 && cron.day == -1 {
+			//rolled over, add a day
+			n := nextRun.AddDate(0, 0, 1)
+			if n.Day() > 1 || cron.mon == -1 {
+				// either month didn't change, or wasn't pinned, so keep
+				nextRun = n
+			}
+		}
+	}
+
+	if !nextRun.After(now) && cron.day == -1 {
+		// try the next day
+		n := nextRun.AddDate(0, 0, 1)
+		if n.Day() > 1 || cron.mon == -1 {
+			// either month didn't change, or wasn't pinned
+			nextRun = n
+			nextRun = time.Date(nextRun.Year(), 
+				time.Month(Iif(cron.mon > -1, cron.mon, int(nextRun.Month()))), 
+				nextRun.Day(), 
+				Iif(cron.hour > -1, cron.hour, 0),  // use hour if specified, else the start of the next day
+				Iif(cron.min > -1, cron.min, 0), // use minute if specified, else the start of the next hour
+				0, 0, now.Location(),
+			)
+		}
+	}
+
+	if !nextRun.After(now) && cron.mon == -1 {
+		// try the next month
+		nextRun = nextRun.AddDate(0, 1, 0)
+		nextRun = time.Date(nextRun.Year(), nextRun.Month(), 
+			Iif(cron.day > -1, cron.day, 1),  // use day if specified, else the start of the next month
+			Iif(cron.hour > -1, cron.hour, 0),  // use hour if specified, else the start of the next day
+			Iif(cron.min > -1, cron.min, 0), // use minute if specified, else the start of the next hour
+			0, 0, now.Location(),
+		)
+	}
+
+	if !nextRun.After(now) {
+		// try the next year
+		nextRun = time.Date(
+			nextRun.Year()+1, 
+			time.Month(Iif(cron.mon > -1, cron.mon, 1)), 
+			Iif(cron.day > -1, cron.day, 1),  // use day if specified, else the start of the next month
+			Iif(cron.hour > -1, cron.hour, 0),  // use hour if specified, else the start of the next day
+			Iif(cron.min > -1, cron.min, 0), // use minute if specified, else the start of the next hour
+			0, 0, now.Location(),
+		)
+	}
+
+	// ignore wday for now
 
 	return nextRun
 }
