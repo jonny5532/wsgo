412bd51abe6e5907f681b62909aa5ad2b7197ccc
Jonny <jonny@jonnybarker.com>
2022-11-21 09:42:37 +0000

    Update build env, bump version to 0.0.7

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 ./build_version.sh 3 6 15 python-3.6
-./build_version.sh 3 7 12 python-3.7
-./build_version.sh 3 8 12 python-3.8-embed
-./build_version.sh 3 9 10 python-3.9-embed
-./build_version.sh 3 10 2 python-3.10-embed
+./build_version.sh 3 7 15 python-3.7
+./build_version.sh 3 8 15 python-3.8-embed
+./build_version.sh 3 9 15 python-3.9-embed
+./build_version.sh 3 10 8 python-3.10-embed
+./build_version.sh 3 11 0 python-3.11-embed
