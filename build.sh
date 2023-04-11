d449bbb83576a2e46fe749138305daaf91fe1622
Jonny <jonny@jonnybarker.com>
2023-04-11 09:04:42 +0100

    Bump to 0.0.10

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 ./build_version.sh 3 7 16 python-3.7
 ./build_version.sh 3 8 16 python-3.8-embed
 ./build_version.sh 3 9 16 python-3.9-embed
-./build_version.sh 3 10 10 python-3.10-embed
-./build_version.sh 3 11 2 python-3.11-embed
+./build_version.sh 3 10 11 python-3.10-embed
+./build_version.sh 3 11 3 python-3.11-embed
