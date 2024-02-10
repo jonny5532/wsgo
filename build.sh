deecdf8d2eb8b2c5b02395547244795e7568dca3
Jonny <jonny@jonnybarker.com>
2024-02-10 10:59:26 +0000

    Update Python versions and add 3.12

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 ./build_version.sh 3 6 15 python-3.6
-./build_version.sh 3 7 16 python-3.7
-./build_version.sh 3 8 16 python-3.8-embed
-./build_version.sh 3 9 16 python-3.9-embed
-./build_version.sh 3 10 11 python-3.10-embed
-./build_version.sh 3 11 3 python-3.11-embed
+./build_version.sh 3 7 17 python-3.7
+./build_version.sh 3 8 18 python-3.8-embed
+./build_version.sh 3 9 18 python-3.9-embed
+./build_version.sh 3 10 13 python-3.10-embed
+./build_version.sh 3 11 8 python-3.11-embed
+./build_version.sh 3 12 2 python-3.12-embed
