a4281b152077590f33df8306bfb626153f1e1849
Jonny <jonny@jonnybarker.com>
2024-02-10 10:59:04 +0000

    Make CacheWriter more comprehensible, and small optimisations

--- wsgo/cache_writer.go
+++ wsgo/cache_writer.go
@@ -20,7 +20,8 @@ var cacheWriterLimit = 1000000
 
 func NewCacheWriter(w http.ResponseWriter) *CacheWriter {
 	return &CacheWriter{
-		writer: w,
+		writer:        w,
+		doneBuffering: skipBuffering(),
 	}
 }
 
@@ -36,7 +37,15 @@ func NewNonCachingCacheWriter(w http.ResponseWriter) *CacheWriter {
 	return &CacheWriter{
 		writer: w,
 		skipCaching:   true,
+		doneBuffering: skipBuffering(),
 	}
+}
+
+func skipBuffering() bool {
+	if responseBufferLength==0 {
+		return true
+	}
+	return false
 }
 
 func (cw *CacheWriter) Header() http.Header {
@@ -46,83 +55,78 @@ func (cw *CacheWriter) Header() http.Header {
 	return cw.writer.Header()
 }
 
-func (cw *CacheWriter) WriteOld(b []byte) (int, error) {
-	var n int
-	var err error
-	if cw.writer != nil {
-		n, err = cw.writer.Write(b)
-	} else {
-		n, err = len(b), nil
+func (cw *CacheWriter) bufferedWrite(b []byte) (int, error, int) {
+	// Write as much of b as possible to the internal buffer. 
+	
+	// When the buffer is full, write it out to the underlying writer (sending
+	// the header first), and then write the rest of b out directly.
+
+	// Returns (amount_written_if_error, error, buffered)
+
+	// How much space is there left in the buffer?
+	remainingBufferSpace := responseBufferLength - len(cw.buf)
+	// How much will we buffer?
+	toBuffer := min(len(b), remainingBufferSpace)
+
+	if toBuffer > 0 {
+		// Add it to the buffer!
+		cw.buf = append(cw.buf, b[:toBuffer]...)
 	}
 
-	if cw.skipCaching {
-		// we've given up caching the response
-	} else if err != nil || len(cw.buf)+n > cacheWriterLimit {
-		// either erroring or got too big to cache, give up
-		cw.skipCaching = true
-		cw.buf = []byte{}
-	} else {
-		cw.buf = append(cw.buf, b[:n]...)
+	if len(cw.buf) >= responseBufferLength {
+		// We have filled the buffer, now write it out
+
+		cw.writer.WriteHeader(cw.statusCode)
+		cw.doneBuffering = true
+		if len(cw.buf) > 0 {
+			_, err := cw.writer.Write(cw.buf)
+			if err != nil {
+				cw.skipCaching = true
+				// Technically we didn't write any of b
+				return 0, err, 0
+			}
+		}
+	}
+	if len(b) > toBuffer {
+		// Still have more left to write out
+
+		if len(cw.buf) + (len(b)-toBuffer) > cacheWriterLimit {
+			// Will overflow cache, so stop caching
+			cw.skipCaching = true
+		}
+
+		// Write out the remaining
+		n, err := cw.writer.Write(b[toBuffer:len(b)])
+		if err != nil {
+			cw.skipCaching = true
+			return n, err, 0
+		}
 	}
 
-	return n, err
+	return 0, nil, toBuffer
 }
 
 func (cw *CacheWriter) Write(b []byte) (int, error) {
-	// TODO - make this simpler
-
 	if cw.finished {
 		return 0, errors.New("Response already finished.")
 	}
 
-	// algorithm:
-
-	// the first responseBufferLength bytes go straight into buf
-	// once we hit responseBufferLength, we:
-	//  - output the entire responseBufferLength
-	//  - write the remainder of b directly
-	// then if we're skipCaching, empty buf and go into straight-through mode
-    // then check whether we've hit cacheWriterLimit, if so, empty buf and enable skipCaching
-    // if not, keep filling buf whilst outputting directly
-
-    cacheStartOffset := 0
+    buffered := 0
 
     if !cw.doneBuffering {
-	    remainingBufferSpace := responseBufferLength - len(cw.buf)
-	    //if remainingBufferSpace > 0 {
-	    	toBuffer := min(len(b), remainingBufferSpace)
-	    	cw.buf = append(cw.buf, b[:toBuffer]...)
-	    	cacheStartOffset = toBuffer
-	    	if len(cw.buf) >= responseBufferLength {
-	    		// have filled the buffer, now write it out
-				cw.writer.WriteHeader(cw.statusCode)
-	    		_, err := cw.writer.Write(cw.buf)
-	    		cw.doneBuffering = true
-	    		if err != nil {
-					cw.skipCaching = true
-		    		// technically we didn't write any of b
-		    		return 0, err
-		    	}
-	    	}
-	    	if len(b) > toBuffer {
-	    		// need to keep caching potentially??
-	    		if len(cw.buf) + (len(b)-toBuffer) > cacheWriterLimit {
-	    			// will overflow cache, stop
-	    			cw.skipCaching = true
-	    		}
+		// We're still buffering, so write b to the buffer. May spill to the 
+		// underlying writer if we exceed the buffer size.
+		n, err, _buffered := cw.bufferedWrite(b)
+		if err != nil {
+			// Writing failed, bail now.
+			return n, err
+		}
 
-	    		// still some unsent b remaining
-	    		n, err := cw.writer.Write(b[toBuffer:len(b)])
-	    		if err != nil {
-					cw.skipCaching = true
-	    			return n, err
-	    		}
-	    	}
-	    //} else {
-    	//	cw.doneBuffering = true
- 		//	cw.writer.WriteHeader(cw.statusCode) //???
-	    //}
+		// Track how much of b got buffered (there might be some left that got 
+		// written out instead).
+		buffered = _buffered
 	} else if cw.writer != nil {
+		// Not buffering, just write out as normal.
 		n, err := cw.writer.Write(b)
 		if err != nil {
 			cw.skipCaching = true
@@ -131,43 +135,23 @@ func (cw *CacheWriter) Write(b []byte) (int, error) {
 	}
 
 	if cw.skipCaching {
-		// we've given up caching the response
-	} else if len(cw.buf) + ( len(b)-cacheStartOffset ) > cacheWriterLimit {
-		// got too big to cache, give up
+		// We've given up caching the response
+	} else if len(cw.buf) + ( len(b)-buffered ) > cacheWriterLimit {
+		// Got too big to cache, give up
 		cw.skipCaching = true
-	} else if len(b) - cacheStartOffset > 0 {
-		//is some remaining in the buffer to cache
-		cw.buf = append(cw.buf, b[cacheStartOffset:len(b)]...)
+	} else if len(b) - buffered > 0 {
+		// We're still caching, and we didn't buffer the whole of b, so buffer 
+		// the rest now for caching purposes.
+		cw.buf = append(cw.buf, b[buffered:len(b)]...)
 	}
 
 	if cw.skipCaching && cw.doneBuffering && len(cw.buf)>0 {
+		// Buffer no longer useful, release the memory now.
 	 	cw.buf = []byte{}
 	}
 
 	return len(b), nil
 }
-
-// func (cw *CacheWriter) Write2(b []byte) (int, error) {
-// 	toBuffer := 0
-// 	if cacheWriterLimit > 0 && !cw.skipCaching {
-// 		toCache = cacheWriterLimit
-// 	}
-// 	if responseBufferLength > 0 && cw.writer!=nil && responseBufferLength > toBuffer {
-// 		toBuffer = responseBufferLength
-// 	}
-
-// 	toBuffer = min(
-// 		toBuffer - len(cw.buf),
-// 		len(b),
-// 	)
-
-// 	if toBuffer > 0 {
-// 		cw.buf = append(cw.buf, b[:toBuffer]...)
-// 	}
-
-
-
-// }
 
 func (cw *CacheWriter) Flush() error {
 	if cw.finished {
@@ -195,6 +179,8 @@ func (cw *CacheWriter) WriteHeader(statusCode int) {
 		return
 	}
 	if cw.doneBuffering && cw.writer != nil {
+		// Write header out now (if we were still buffering it'd get written 
+		// when the buffer hits full).
 		cw.writer.WriteHeader(statusCode)
 	}
 	contentLength, err := strconv.Atoi(cw.Header().Get("Content-length"))
@@ -202,10 +188,10 @@ func (cw *CacheWriter) WriteHeader(statusCode int) {
 	cw.statusCode = statusCode
 
 	if statusCode != 200 && statusCode != 204 && statusCode != 301 {
-		// probably an error of some kind, skip
+		// Probably an error of some kind, skip
 		cw.skipCaching = true
 	} else if err == nil && contentLength > cacheWriterLimit {
-		// too big to cache, skip
+		// Will be too big to cache, don't bother trying
 		cw.skipCaching = true
 	} else {
 //		cw.statusCode = statusCode
