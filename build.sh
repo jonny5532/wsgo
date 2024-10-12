bf2f442ea1c604f3c66503d744a0915098876d45
Jonny <jonny@jonnybarker.com>
2024-10-12 12:54:44 +0100

    Add Python 3.13 build, bump version

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@
 ./build_version.sh 3 9 20 python-3.9-embed
 ./build_version.sh 3 10 15 python-3.10-embed
 ./build_version.sh 3 11 10 python-3.11-embed
-./build_version.sh 3 12 6 python-3.12-embed
+./build_version.sh 3 12 7 python-3.12-embed
+./build_version.sh 3 13 0 python-3.13-embed
--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,5 @@
 ./build_version.sh 3 9 20 python-3.9-embed "LD_LIBRARY_PATH=/usr/local/lib /usr/local/bin/python3.? -m tests"
 ./build_version.sh 3 10 15 python-3.10-embed "LD_LIBRARY_PATH=/usr/local/lib /usr/local/bin/python3.1? -m tests"
 ./build_version.sh 3 11 10 python-3.11-embed "LD_LIBRARY_PATH=/usr/local/lib /usr/local/bin/python3.1? -m tests"
-./build_version.sh 3 12 6 python-3.12-embed "LD_LIBRARY_PATH=/usr/local/lib /usr/local/bin/python3.1? -m tests"
-./build_version.sh 3 13 0rc2 python-3.13-embed "LD_LIBRARY_PATH=/usr/local/lib /usr/local/bin/python3.1? -m tests"
+./build_version.sh 3 12 7 python-3.12-embed "LD_LIBRARY_PATH=/usr/local/lib /usr/local/bin/python3.1? -m tests"
+./build_version.sh 3 13 0 python-3.13-embed "LD_LIBRARY_PATH=/usr/local/lib /usr/local/bin/python3.1? -m tests"
